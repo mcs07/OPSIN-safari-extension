5ff01526661982cf0fe1a5195b4c259fbb7c8359
Matt Swain <m.swain@me.com>
2012-04-08 13:31:50 +0100

    Updated to use jquery UI dialog

--- opsin.safariextension/js/global.js
+++ opsin.safariextension/js/global.js
@@ -1,35 +1,28 @@
-safari.application.addEventListener("command", performCommand, false);
-safari.application.addEventListener("validate", validateCommand, false);
-
-// Called when the context menu item is clicked
-function performCommand(event) {
-	if (event.command !== "opsin")
+function performCommand(e) {
+	if (e.command !== 'opsin')
 		return;
-
-	// Get selection from userInfo
-	var selection = event.userInfo;
+	var selection = e.userInfo;
 	safari.application.activeBrowserWindow.activeTab.page.dispatchMessage("searchResults", selection);
 }
 
-// Called whenever context menu is displayed
-function validateCommand(event) {
-
-	// Get selection from userInfo
-	var selection = event.userInfo;
-	
-	// Remove item from context menu if criteria aren't satisfied
-	if (event.command !== "opsin" || selection===undefined) {
+function validateCommand(e) {
+	var selection = e.userInfo;
+	if (e.command !== 'opsin' || selection===undefined) {
 		return;
 	}
 	if (selection.length == 0 || !selection) {
-		event.target.disabled = true;
+		e.target.disabled = true;
 	}
-	
-	// Truncate context menu item if too long
 	if (selection.length > 25) {
 		selection = selection.substr(0,25);
-		selection = selection.replace(/^\s+|\s+$/g,"");
+		selection = selection.replace(/^\s+|\s+$/g,'');
 		selection = selection + '...'
 	}
-	event.target.title = 'Display "'+selection+'" using OPSIN'; 
+	e.target.title = 'Display "'+selection+'" using OPSIN'; 
 }
+
+const app = safari.application,
+	  ext  = safari.extension;
+app.addEventListener('command', performCommand, false);
+app.addEventListener('validate', validateCommand, false);
+app.addEventListener('message', handleMessage, false);
