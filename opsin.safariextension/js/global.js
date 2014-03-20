2d356cf38cafbad996ef6b7d9657df0acab86da7
Matt Swain <m.swain@me.com>
2014-03-20 16:27:24 +0000

    Update dialog style

--- opsin.safariextension/js/global.js
+++ opsin.safariextension/js/global.js
@@ -1,24 +1,22 @@
 function performCommand(e) {
-	if (e.command !== 'opsin')
-		return;
-	var selection = e.userInfo;
-	app.activeBrowserWindow.activeTab.page.dispatchMessage('searchResults', selection);
+	if (e.command !== 'opsin') return;
+	app.activeBrowserWindow.activeTab.page.dispatchMessage('searchResults', e.userInfo);
 }
 
 function validateCommand(e) {
 	var selection = e.userInfo;
-	if (e.command !== 'opsin' || selection===undefined) {
+	if (e.command !== 'opsin' || selection === undefined) {
 		return;
 	}
+	// Only show OPSIN context menu item if there is some selected text
 	if (selection.length == 0 || !selection) {
 		e.target.disabled = true;
 	}
+	// Truncate the menu item text if the selection is over 25 characters
 	if (selection.length > 25) {
-		selection = selection.substr(0,25);
-		selection = selection.replace(/^\s+|\s+$/g,'');
-		selection = selection + '...'
+		selection = selection.substr(0, 25).replace(/^\s+|\s+$/g,'') + '...';
 	}
-	e.target.title = 'Display "'+selection+'" using OPSIN'; 
+	e.target.title = 'Resolve "'+selection+'" using OPSIN';
 }
 
 const app = safari.application;
--- opsin.safariextension/js/injected.js
+++ opsin.safariextension/js/injected.js
@@ -1,59 +1,76 @@
-var locX, locY;
+var locX, locY, locZ = 0;
 
-function contextMessage(e) {
-	locX = e.clientX + 10;
-    locY = e.clientY + 10;
-	var sel = '';
-	sel = window.parent.getSelection()+'';
-	sel = sel.replace(/^\s+|\s+$/g,'');
-	safari.self.tab.setContextMenuEventUserInfo(e, sel);
+function handleContextMenu(e) {
+    // Get mouse position, but limit to page size constraints
+    locX = Math.min(e.pageX + 10, $(document).width() - 200);
+    locY = Math.min(e.pageY + 10, $(document).height() - 300);
+    // Get selection and set as userInfo for global page
+    var sel = '';
+    sel = window.parent.getSelection()+'';
+    sel = sel.replace(/^\s+|\s+$/g,'');
+    safari.self.tab.setContextMenuEventUserInfo(e, sel);
 }
 
 function handleMessage(msg) {
+	if (window !== window.top) return;
 	if (msg.name === 'searchResults') {
-		openWindow(msg.message);
-	}
+        displayResult(msg.message);
+    }
 }
 
-function openWindow(msg) {
-	if (window !== window.top) 
-		return;
+function displayResult(msg) {
 	if ($('link[href^="safari-extension://com.matt-swain.opsin"][href$="css/opsin.css"]').length == 0) {
-		$('<link rel="stylesheet" href="'+safari.extension.baseURI+'css/opsin.css">').appendTo('head');
+        $('<link rel="stylesheet" href="'+safari.extension.baseURI+'css/opsin.css">').appendTo('head');
 	}
-	var $popupDiv = $('<div/>').appendTo('body'),
-		imgString = '<img src="http://opsin.ch.cam.ac.uk/opsin/'+encodeURIComponent(msg)+'.png">',
-		$img = $(imgString).appendTo($popupDiv);
-	
-	$popupDiv.dialog({
-		position: [locX,locY],
-		width: 154,
-		title: 'OPSIN',
-		buttons: [
-			{text: 'SMILES', 'class':'__opsin-dialog-button', click: function() { 
-				location.href = 'http://opsin.ch.cam.ac.uk/opsin/'+encodeURIComponent(msg)+'.smi';
-			}},
-			{text: 'InChi', 'class':'__opsin-dialog-button', click: function() {
-				location.href = 'http://opsin.ch.cam.ac.uk/opsin/'+encodeURIComponent(msg)+'.inchi';
-			}},
-			{text: 'CML', 'class':'__opsin-dialog-button', click: function() {
-				location.href = 'http://opsin.ch.cam.ac.uk/opsin/'+encodeURIComponent(msg)+'.cml';
-			}},
-		]
-	});
-	
-	$img.one('error', function() { 
-		$(this).replaceWith($('<div>No Results Found</div>'));
-		$popupDiv.dialog({
-			buttons: [
-				{text: 'Close', 'class':'__opsin-dialog-button', click: function() {
-					$(this).dialog('close'); 
-				}},
-			]
-		});
-	});
-	
+    var dialog = $('<div class="opsin-safari-extension" />'),
+        title = $('<div class="opsse-title">OPSIN</div>')
+        main = $('<div class="opsse-mol" />'),
+        close = $('<button type="button" class="opsse-close">×</button>'),
+        img = $('<img src="http://opsin.ch.cam.ac.uk/opsin/'+encodeURIComponent(msg)+'.png" title="'+msg+'">'),
+        smiles = $('<button type="button" title="Save SMILES file" class="opsse-save">SMILES</button>'),
+        inchi = $('<button type="button" title="Save InChI file" class="opsse-save">InChI</button>'),
+        cml = $('<button type="button" title="Save CML file" class="opsse-save">CML</button>');
+    smiles.click(function() { location.href = 'http://opsin.ch.cam.ac.uk/opsin/'+encodeURIComponent(msg)+'.smi'; });
+    inchi.click(function() { location.href = 'http://opsin.ch.cam.ac.uk/opsin/'+encodeURIComponent(msg)+'.inchi'; });
+    cml.click(function() { location.href = 'http://opsin.ch.cam.ac.uk/opsin/'+encodeURIComponent(msg)+'.cml'; });
+    close.click(function() { $(this).parent().fadeOut(); });
+    dialog.append(close).append(title).append(main);
+    main.append(img).append(smiles).append(inchi).append(cml);
+    img.error(function() {
+    	main.remove();
+    	dialog.append('<div class="opsse-noresults">No Results Found</div>');
+    })
+    dialog.css({top: locY, left: locX, display: 'none'});
+    dialog.drags({handle: '.opsse-title'})
+    dialog.appendTo('body').fadeIn();
 }
 
-document.addEventListener('contextmenu', contextMessage, false);
+// Draggable code
+(function($) {
+    $.fn.drags = function(opt) {
+        opt = $.extend({cursor:'move'}, opt);
+        var $el = this.find(opt.handle);
+        return $el.css('cursor', opt.cursor).on('mousedown', function(e) {
+            var $drag = $(this).addClass('opsse-active-handle').parent().addClass('opsse-draggable');
+            locZ = Math.max(5001, locZ+1);
+            var drg_h = $drag.outerHeight(),
+                drg_w = $drag.outerWidth(),
+                pos_y = $drag.offset().top + drg_h - e.pageY,
+                pos_x = $drag.offset().left + drg_w - e.pageX;
+            $drag.css('z-index', locZ).parents().on('mousemove', function(e) {
+                $('.opsse-draggable').offset({
+                    top:e.pageY + pos_y - drg_h,
+                    left:e.pageX + pos_x - drg_w
+                }).on('mouseup', function() {
+                    $(this).removeClass('opsse-draggable');
+                });
+            });
+            e.preventDefault(); // disable selection
+        }).on('mouseup', function() {
+            $(this).removeClass('opsse-active-handle').parent().removeClass('opsse-draggable');
+        });
+    }
+})(jQuery);
+
+document.addEventListener('contextmenu', handleContextMenu, false);
 safari.self.addEventListener('message', handleMessage, false);
